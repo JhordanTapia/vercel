11614e457576bdadd9ca8c5c36e68e8e76d063dc
JhordanTapia <jhordantapiaespinoza@gmail.com>
2025-06-09 12:11:37 -0500

    internacional.php

--- api/secciones/internacional.php
+++ api/secciones/internacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Tensiones Geopolíticas y su Impacto en el Comercio Internacional",
+"autor" => "Dr. Luis Fernández",
+"resumen" => "El artículo examina cómo los conflictos entre potencias globales afectan las dinámicas del comercio internacional, analizando casos recientes como la guerra comercial entre EE.UU. y China, y su repercusión en economías emergentes.",
 ];
 ?>
