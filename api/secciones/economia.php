d13a07b1f925e69731317e10ea88eabafc2c38d9
JhordanTapia <jhordantapiaespinoza@gmail.com>
2025-06-09 12:13:47 -0500

    economia.php

--- api/secciones/economia.php
+++ api/secciones/economia.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+"titulo" => "Inflación y Recuperación Económica en América Latina post-COVID",
+    "autor" => "Eco. Javier Salinas",
+    "resumen" => "El informe explora cómo los países latinoamericanos están enfrentando la inflación tras la pandemia, evaluando las políticas fiscales y monetarias adoptadas para impulsar la reactivación económica sostenible."
 ];
 ?>
