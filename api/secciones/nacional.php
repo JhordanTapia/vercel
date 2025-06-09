69348370c045325a2f697c9ead3f1d96600baed0
JhordanTapia <jhordantapiaespinoza@gmail.com>
2025-06-09 12:13:23 -0500

    nacional.php

--- api/secciones/nacional.php
+++ api/secciones/nacional.php
@@ -4,8 +4,8 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Desafíos de la Educación Pública en el Perú Rural",
+    "autor" => "Mg. Carla Huamán",
+    "resumen" => "Este artículo analiza las principales barreras que enfrentan los estudiantes en zonas rurales del Perú, incluyendo infraestructura deficiente, escasez de docentes capacitados y limitado acceso a tecnología."
 ];
 ?>
